0af87f846b094dcb6f92b85195872c36dd9aba80
Thomas Harold <tgharold@gmail.com>
2015-09-04 13:14:00 -0400

    add CreateAGenerator()

--- tests/Core.Tests/TestHelper.cs
+++ tests/Core.Tests/TestHelper.cs
@@ -63,5 +63,30 @@ namespace RimDev.Supurlative.Tests
             return new UrlGenerator(request, supurlativeOptions);
         }
 
+        public static Generator CreateAGenerator(
+            string baseUrl,
+            string routeName,
+            string routeTemplate,
+            object routeDefaults = null,
+            object routeConstraints = null,
+            SupurlativeOptions supurlativeOptions = null
+            )
+        {
+            HttpRouteCollection routes = new HttpRouteCollection();
+            routes.MapHttpRoute(
+                routeName,
+                routeTemplate,
+                defaults: routeDefaults,
+                constraints: routeConstraints
+                );
+            HttpConfiguration configuration = new HttpConfiguration(routes);
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(baseUrl),
+                Method = HttpMethod.Get
+            };
+            request.SetConfiguration(configuration);
+            return new Generator(request, supurlativeOptions);
+        }
     }
 }
