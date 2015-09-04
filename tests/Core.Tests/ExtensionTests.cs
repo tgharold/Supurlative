d9b60fc43322344a5136a2c89aa190278d09a046
Thomas Harold <tgharold@gmail.com>
2015-09-04 14:00:18 -0400

    WebApiHelper is now only used by ExtensionTests

--- tests/Core.Tests/ExtensionTests.cs
+++ tests/Core.Tests/ExtensionTests.cs
@@ -36,4 +36,31 @@ namespace RimDev.Supurlative.Tests
             Assert.True(names.Count > 0);
         }
     }
+
+    private static class WebApiHelper
+    {
+        public static HttpRequestMessage GetRequest()
+        {
+            var routes = new HttpRouteCollection();
+
+            routes.MapHttpRoute("foo.show", "foo/{id}");
+            routes.MapHttpRoute("foo.one.two", "foo/{one}/{two}");
+            routes.MapHttpRoute("bar.show", "bar/{id}", defaults: new { id = RouteParameter.Optional });
+            routes.MapHttpRoute("bar.one.two", "bar/{one}/{two}",
+                defaults: new { one = RouteParameter.Optional, two = RouteParameter.Optional });
+            routes.MapHttpRoute("constraint", "constraints/{id:int}", null, new { id = @"\d+" });
+
+            var configuration = new HttpConfiguration(routes);
+
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://localhost:8000/"),
+                Method = HttpMethod.Get
+            };
+
+            request.SetConfiguration(configuration);
+
+            return request;
+        }
+    }
 }
